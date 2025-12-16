ed2739ec9813ebe14efc30e0e41f326133ddadec
mohdtajul <mohdtajul473@gmail.com>
2025-12-16 16:39:46 +0530

    added validateReview middleware

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/ExpressError.js")
-const {listingSchema} = require("./schema.js")
+const {listingSchema, reviewSchema} = require("./schema.js")
 const Review = require("./models/reviews.js")
 
 require('dotenv').config();
@@ -38,6 +38,16 @@ app.get("/",(req,res)=>{
 
 const validationListing = (req,res,next)=>{
     let error = listingSchema.validate(req.body)
+        if(error){
+            let errMsg = error.details.map((el)=> el.message).join(",")
+            throw new ExpressError(404,errMsg)
+        }else{
+            next();
+        }
+}
+
+const validateReview = (req,res,next)=>{
+    let error = reviewSchema.validate(req.body)
         if(error){
             let errMsg = error.details.map((el)=> el.message).join(",")
             throw new ExpressError(404,errMsg)
@@ -113,7 +123,7 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
 )
 
 // reviews route
-app.post("/listings/:id/reviews", async(req,res)=>{
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
     let listing = await Listing.findById(req.params.id)
     let newReview = new Review(req.body.review)
     listing.reviews.push(newReview)
@@ -124,6 +134,7 @@ app.post("/listings/:id/reviews", async(req,res)=>{
     console.log(listing.reviews)
     res.redirect(`/listings/${listing._id}`)
 })
+)
 
 // jab koi route match na krega tb chlega ye New route 
 app.use((req, res, next) => {
