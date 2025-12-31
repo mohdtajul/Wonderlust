5006f1a4b70bc2243f6845d1201366168c086a30
mohdtajul <mohdtajul473@gmail.com>
2025-12-31 19:43:29 +0530

    added flash message new listing created

--- routes/listing.js
+++ routes/listing.js
@@ -41,6 +41,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/new",validationListing, wrapAsync(async (req,res,next)=>{
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created")
         res.redirect("/listings")
     })
 );
