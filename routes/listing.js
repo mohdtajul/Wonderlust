a2ef8e9c20c49b787146ce67d60b7a98aec82ec9
mohdtajul <mohdtajul473@gmail.com>
2026-01-01 11:31:42 +0530

    added flash msg to update and delete api calls

--- routes/listing.js
+++ routes/listing.js
@@ -66,7 +66,7 @@ router.put("/:id",validationListing, wrapAsync(async(req,res)=>{
         location: listing.location,
         country: listing.country
    })
-  
+    req.flash("success", "Listing Updated")
     res.redirect("/listings")
 
     })
@@ -76,6 +76,7 @@ router.put("/:id",validationListing, wrapAsync(async(req,res)=>{
 router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params
     await Listing.findByIdAndDelete(id)
+    req.flash("success", "Listing Deleted")
     res.redirect("/listings")
 
     })
