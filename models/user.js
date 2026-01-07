38640c22d2cf290983794fc32058ff2e23c1fee4
mohdtajul <mohdtajul473@gmail.com>
2026-01-07 21:31:01 +0530

    Added user authentication model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+    },
+})
+
+User.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User", userSchema)
+
