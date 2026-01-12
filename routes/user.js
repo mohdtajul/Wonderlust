16699a15f76be16f0c71ac653174eeded0b72840
mohdtajul <mohdtajul473@gmail.com>
2026-01-12 22:30:27 +0530

    added logout route

--- routes/user.js
+++ routes/user.js
@@ -41,4 +41,15 @@ router.post(
         res.render("/listings")
 })
 
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "you are logged out!")
+        res.redirect("/listigns")
+
+    })
+})
+
 module.exports = router;
